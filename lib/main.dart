7546e49d4c52177629a05ae80142a4b4dc11b1a1
Daniel Cunha <danielcunha54@gmail.com>
2021-08-01 16:37:52 -0300

    feat: handle notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_notification/pages/home.dart';
+import 'package:flutter_firebase_notification/pages/loading.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Cloud Messaging',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: const HomePage(),
-      debugShowCheckedModeBanner: false,
+    return FutureBuilder(
+      future: _initialization,
+      builder: (ctx, snapshot) {
+        return MaterialApp(
+          title: 'Firebase Cloud Messaging',
+          theme: ThemeData(primarySwatch: Colors.indigo),
+          home: snapshot.connectionState == ConnectionState.done
+              ? const HomePage()
+              : const LoadingPage(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    _initialize();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,5 +23,35 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
     );
+  }
+
+  Future _initialize() async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    debugPrint('User granted permission: ${settings.authorizationStatus}');
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      await messaging.subscribeToTopic('developers');
+      final token = await messaging.getToken();
+      debugPrint('Token: $token');
+
+      FirebaseMessaging.onMessage.listen((message) {
+        if (message.notification != null) {
+          showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+              title: Text(message.notification!.title ?? 'Notification'),
+              content: Text(message.notification!.body ?? 'No content.'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(ctx).pop(),
+                  child: const Text('Ok'),
+                ),
+              ],
+            ),
+          );
+        }
+      });
+    }
   }
 }
